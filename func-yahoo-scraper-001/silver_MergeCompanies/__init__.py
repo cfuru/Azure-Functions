2c0f4eee389b7549423128e1a63e88b0bf715e2d
cfuru <cfuru@kth.se>
2023-01-09 13:19:58 +0100

    adding silver merging company function

---
+++ func-yahoo-scraper-001/silver_MergeCompanies/__init__.py
@@ -0,0 +1,58 @@
+#sys.path.insert(0, 'C:/Users/chris/Documents/GitHub/AzureFunctions/AzureFunctions')
+import datetime
+import logging
+import time
+from datetime import date
+import os
+from io import BytesIO
+import pandas as pd
+
+import azure.functions as func
+from shared_code import utils
+
+def main(mytimer: func.TimerRequest) -> None:
+
+    azure_utils = utils.AzureUtils()
+    dataCleaning_utils = utils.DataCleaning()
+    
+    secret_client = azure_utils.initialize_key_vault()
+    sa_secret = azure_utils.get_key_vault_secret(secret_client, 'sa-secret')
+    sa_name = azure_utils.get_key_vault_secret(secret_client, 'sa-name')
+    azure_utils.initialize_storage_account_ad(sa_secret.value, sa_name.value)
+    
+    selected_cols = [
+        'Ticker',
+        'zip', 
+        'sector', 
+        'longBusinessSummary',
+        'city',
+        'country', 
+        'website',
+        'address1',
+        'industry',
+        'longName',
+        'financialCurrency',
+        'exchange',
+        'isEsgPopulated',
+        'quoteType'
+    ]
+    
+    df_cleaning = (
+        azure_utils.ingest_bronze_data(f"fundamentals/{date.today()}")
+        .pipe(dataCleaning_utils.pivot_fundamentals_dataframe)
+        .pipe(dataCleaning_utils.select_dataframe_columns, selected_cols)
+    )
+    
+    df_cleaning = df_cleaning.astype(str).set_index("Ticker")
+
+    parquet_file = df_cleaning.to_parquet(index = False)
+
+    azure_utils.upload_blob(parquet_file, f"silver/company", f"company_{date.today()}.parquet")
+    
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function ran at %s', utc_timestamp)
